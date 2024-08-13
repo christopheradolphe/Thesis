4cd924e2512a36d394175470efc865e6bced3f24
christopheradolphe <ckwadolphe@gmail.com>
2024-08-13 19:43:35 -0400

    Add stationarity check and remove rows with no vix data

--- Problem_Set/VIX.py
+++ Problem_Set/VIX.py
@@ -1,8 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from statsmodels.tsa.stattools import adfuller
 
 #Loading excel VIX Data
 vix_data = pd.read_csv('vixdata.csv', parse_dates=['dt'], index_col='dt')
 
-vix_data.plot(title = "VIX data")
-plt.show()
+#Plot the vix data
+# vix_data.plot(title = "VIX data")
+# plt.show()
+
+#Remove empty vix data
+if vix_data['vix'].isnull().sum() != 0:
+  vix_data.dropna(subset= ['vix'], inplace=True)
+
+print(vix_data['vix'].isnull().sum())
+
+#Stationarity Check
+stationarity_check = adfuller(vix_data['vix'])
+print("ADF Statistic" + {stationarity_check[0]})
