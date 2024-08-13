a91ab4421097c202f206882ec9fd2fd07a36fba1
christopheradolphe <ckwadolphe@gmail.com>
2024-08-13 19:52:44 -0400

    Process ADF test data

--- Problem_Set/VIX.py
+++ Problem_Set/VIX.py
@@ -13,8 +13,11 @@ vix_data = pd.read_csv('vixdata.csv', parse_dates=['dt'], index_col='dt')
 if vix_data['vix'].isnull().sum() != 0:
   vix_data.dropna(subset= ['vix'], inplace=True)
 
-print(vix_data['vix'].isnull().sum())
-
 #Stationarity Check
 stationarity_check = adfuller(vix_data['vix'])
-print("ADF Statistic" + {stationarity_check[0]})
+print("ADF Statistic: ", stationarity_check[0])
+print("p-value: ", stationarity_check[1])
+if stationarity_check[1] > 0.05:
+  print("Data does not fulfill stationarity")
+  exit(-1)
+
