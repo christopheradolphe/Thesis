1ff70171da662426edea23a83e42e84461f8f15a
christopheradolphe <ckwadolphe@gmail.com>
2024-08-13 19:22:27 -0400

    Use matplotlib to plot the time series data

--- Problem_Set/VIX.py
+++ Problem_Set/VIX.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 #Loading excel VIX Data
 vix_data = pd.read_csv('vixdata.csv', parse_dates=['dt'], index_col='dt')
 
+vix_data.plot(title = "VIX data")
+plt.show()
