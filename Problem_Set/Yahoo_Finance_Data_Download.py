56b87005d5229d7262aa0e9875c7a954b9aed83b
christopheradolphe <ckwadolphe@gmail.com>
2024-08-15 23:36:45 -0400

    Change interval for downloaded data from 1 month to 1 day

--- Problem_Set/Yahoo_Finance_Data_Download.py
+++ Problem_Set/Yahoo_Finance_Data_Download.py
@@ -7,7 +7,7 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 
 for ticker in tickers:
     # Downloading data for each ticker
-    data = yf.download(ticker, start=start_date, interval='1mo')
+    data = yf.download(ticker, start=start_date, interval='1d')
     
     # Create a file name with the ticker symbol
     file_name = f"{ticker.replace('^', '')}_data.csv"  # Remove '^' from S&P 500 ticker for a cleaner file name
