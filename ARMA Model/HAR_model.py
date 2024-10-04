ed1b54057c0b7713b46d5a9688bd79c73ff1d5b5
christopheradolphe <ckwadolphe@gmail.com>
2024-10-04 10:27:38 -0400

    Add function to forecast using HAR model with constant exogenous variables (to be updated with forecasts for these variables

--- ARMA Model/HAR_model.py
+++ ARMA Model/HAR_model.py
@@ -20,35 +20,102 @@ def load():
         loaded_model = pickle.load(f)
     return loaded_model
 
-def forecast_har_model(training_data, start_date, end_date, forecast_horizons=range(7, 31)):
+def generate_har_forecasts(har_model, data, start_date, end_date, forecast_horizons=range(1, 35)):
+    """
+    Generate 34-day forecasts using the HAR model.
+
+    Parameters:
+    - har_model: Trained HAR model (statsmodels OLSResults object).
+    - data: DataFrame containing the historical data.
+    - start_date: Start date for forecasting.
+    - end_date: End date for forecasting.
+    - forecast_horizons: Range of forecast horizons (default is 1 to 34).
+
+    Returns:
+    - forecasts_df: DataFrame containing forecasts for each date and horizon.
+    """
     forecasts = []
-    test_dates = training_data[start_date:end_date].index
+    test_dates = data[start_date:end_date].index
+
+    # Extract model coefficients
+    params = har_model.params
+    const = params.get('const', 0)
+    beta_vix_t1 = params.get('VIX_t-1', 0)
+    beta_vix_t5 = params.get('VIX_t-5', 0)
+    beta_vix_t22 = params.get('VIX_t-22', 0)
+    beta_sp_ret_t1 = params.get('S&P Returns_t-1', 0)
+    beta_volume_t1 = params.get('Volume_t-1', 0)
+    beta_termspread_t1 = params.get('TermSpread_t-1', 0)
+
     for t in test_dates:
-        available_data = training_data[:t]
-        model = train(available_data)
-                
-        steps_ahead = max(forecast_horizons)
-        forecast = model.predict(steps=steps_ahead)
+        # Get data up to date t
+        available_data = data.loc[:t]
 
-        # Get forecasted mean values
-        forecast_mean = forecast.predicted_mean
+        # Ensure there is enough data for lagged variables
+        if len(available_data) < 22:
+            continue  # Skip if not enough data
 
-        # Get forecast dates
-        forecast_index = pd.date_range(start=t, periods=steps_ahead + 1, freq='B')[1:]
+        # Initialize forecast dictionary for date t
+        daily_forecast = {'Date': t}
 
-        daily_forecasts = {'Date' : t}
-        # Collect forecasts for specified horizons
-        for h in forecast_horizons:
-            if h - 1 < len(forecast_mean):
-                forecast_value = forecast_mean.iloc[h - 1]
-                # Get actual value if available
-                daily_forecasts[f'{h}'] = forecast_value
+        # Initialize lists to store forecasted VIX values
+        forecasted_VIX = []
+        VIX_history = available_data['Close'].tolist()
+
+        # Prepare initial lagged VIX values
+        for h in range(1, max(forecast_horizons) + 1):
+            # Get lagged VIX values
+            idx_t_h_minus_1 = -1 + h - 1
+            idx_t_h_minus_5 = -1 + h - 5
+            idx_t_h_minus_22 = -1 + h - 22
+
+            # VIX_t-1
+            if idx_t_h_minus_1 < 0:
+                VIX_t1_h = VIX_history[idx_t_h_minus_1]
             else:
-                break
+                VIX_t1_h = forecasted_VIX[idx_t_h_minus_1]
 
-        forecasts.append(daily_forecasts)
+            # VIX_t-5
+            if idx_t_h_minus_5 < 0:
+                VIX_t5_h = VIX_history[idx_t_h_minus_5]
+            else:
+                VIX_t5_h = forecasted_VIX[idx_t_h_minus_5]
 
-    
+            # VIX_t-22
+            if idx_t_h_minus_22 < 0:
+                VIX_t22_h = VIX_history[idx_t_h_minus_22]
+            else:
+                VIX_t22_h = forecasted_VIX[idx_t_h_minus_22]
+
+            # Handle exogenous variables (assuming they remain constant)
+            SP_return_t1 = available_data['S&P Returns_t-1'].iloc[-1]
+            Volume_t1 = available_data['Volume_t-1'].iloc[-1]
+            TermSpread_t1 = available_data['TermSpread_t-1'].iloc[-1]
+
+            # Compute forecast using the HAR model equation
+            y_pred = (const +
+                      beta_vix_t1 * VIX_t1_h +
+                      beta_vix_t5 * VIX_t5_h +
+                      beta_vix_t22 * VIX_t22_h +
+                      beta_sp_ret_t1 * SP_return_t1 +
+                      beta_volume_t1 * Volume_t1 +
+                      beta_termspread_t1 * TermSpread_t1)
+
+            # Append the forecasted value
+            forecasted_VIX.append(y_pred)
+
+            # Store forecasts for specified horizons
+            if h in forecast_horizons:
+                daily_forecast[str(h)] = y_pred
+
+        forecasts.append(daily_forecast)
+
+    # Convert forecasts to DataFrame
     forecasts_df = pd.DataFrame(forecasts)
-    forecasts_df.to_csv('ARMA Forecasts', index=True)
+    forecasts_df.set_index('Date', inplace=True)
+
+    # Reorder columns
+    column_order = [str(h) for h in forecast_horizons]
+    forecasts_df = forecasts_df[column_order]
+
     return forecasts_df
