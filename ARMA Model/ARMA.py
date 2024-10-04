21c9a67cd0b8a41550616e329312ae3ad4c18560
christopheradolphe <ckwadolphe@gmail.com>
2024-10-03 23:36:47 -0400

    Add in argparse to create api like function calls for getting data and training the models

--- ARMA Model/ARMA.py
+++ ARMA Model/ARMA.py
@@ -6,18 +6,9 @@ import yfinance as yf
 from pandas_datareader import data as web
 import pickle
 import statsmodels.api as sm
+import argparse
 
 def load_vix_data(start_date='2004-01-01', end_date='2023-12-31'):
-    """
-    Load historical VIX data from Yahoo Finance.
-    
-    Parameters:
-    - start_date: str, start date for the VIX data.
-    - end_date: str, end date for the VIX data.
-    
-    Returns:
-    - vix_data: Series, containing the VIX close prices.
-    """
     vix_data = yf.download('^VIX', start=start_date, end=end_date)
     vix_data = vix_data['Close'].dropna()  # Keep only the 'Close' column and drop NaN values
     return vix_data
@@ -37,30 +28,7 @@ def load_term_spread(start_date='2004-01-01', end_date='2023-12-31'):
     term_spread.name = 'TermSpread'
     return term_spread
 
-
-def prepare_data(start_date='2004-01-01', end_date='2023-12-31'):
-    # Load data
-    vix_data = load_vix_data(start_date, end_date)
-    sp500_data = load_sp500_data(start_date, end_date)
-    term_spread = load_term_spread(start_date, end_date)
-    
-    # Combine data into a single DataFrame
-    data = pd.concat([vix_data, sp500_data, term_spread], axis = 1)
-    
-    # Drop missing values
-    data = data.dropna()
-    return data
-
 def HAR_data_preparation(data):
-    """
-    Prepare the data for the HAR model by computing lagged variables.
-    
-    Parameters:
-    - data: DataFrame with columns 'VIX_Close', 'SP_Return', 'SP_Volume', 'Term_Spread'
-    
-    Returns:
-    - data: Prepared DataFrame
-    """
     # Compute lagged VIX variables
     data['VIX_t-1'] = data['Close'].shift(1)
     data['VIX_t-5'] = data['Close'].rolling(window=5).mean().shift(1)
@@ -73,6 +41,23 @@ def HAR_data_preparation(data):
     
     # Drop rows with NaN values
     data = data.dropna()
+    return data
+
+def get_latest_data(start_date='2004-01-01', end_date='2023-12-31'):
+    # Load data
+    vix_data = load_vix_data(start_date, end_date)
+    sp500_data = load_sp500_data(start_date, end_date)
+    term_spread = load_term_spread(start_date, end_date)
+    
+    # Combine data into a single DataFrame
+    data = pd.concat([vix_data, sp500_data, term_spread], axis = 1)
+    data = HAR_data_preparation(data)
+    
+    # Drop missing values
+    data = data.dropna()
+    filename = f'Latest_VIX_Data'
+    data.to_csv(filename, index = True)
+    print(f'Latest VIX Data Retrieved and Stored in {filename}.csv')
     return data
 
 def train_ARMA_model(train_data):
@@ -88,20 +73,7 @@ def load_ARMA_model():
         loaded_model = pickle.load(f)
     return loaded_model
 
-def generate_forecasts(vix_data, start_date, end_date, forecast_horizons=range(7, 31)):
-    """
-    Generate forecasts for each date in the out-of-sample period.
-
-    Parameters:
-    - arma_model: Trained ARMA model.
-    - vix_data: Series, full VIX data.
-    - start_date: str, start date of the out-of-sample period.
-    - end_date: str, end date of the out-of-sample period.
-    - forecast_horizons: iterable, forecast horizons to consider.
-
-    Returns:
-    - forecasts_df: DataFrame containing forecasts and actual values.
-    """
+def generate_forecasts(vix_data, start_date, end_date, forecast_horizons=range(7, 34)):
     forecasts = []
     test_dates = vix_data[start_date:end_date].index
 
@@ -139,23 +111,76 @@ def generate_forecasts(vix_data, start_date, end_date, forecast_horizons=range(7
 
         forecasts.append(daily_forecasts)
     forecasts_df = pd.DataFrame(forecasts)
-    forecasts_df.to_csv('ARMA Forecasts 2014', index=True)
+    forecasts_df.to_csv('ARMA Forecasts', index=True)
     return forecasts_df
+
+def fit_har_model(train_data):
+    y = train_data['Close']
+    X = train_data[['VIX_t-1', 'VIX_t-5', 'VIX_t-22', 'S&P Returns_t-1', 'Volume_t-1', 'TermSpread_t-1']]
+    X = sm.add_constant(X)
+    model = sm.OLS(y, X)
+    results = model.fit()
+    return results
+
+def forecast_har_model(start_date, end_date, forecast_horizons=range(7, 31)):
+    forecasts = []
+    test_dates = vix_data[start_date:end_date].index
+    for t in test_dates:
+        available_data = vix_data[:t]
+        model = fit_har_model(available_data)
+                
+        steps_ahead = max(forecast_horizons)
+        forecast = model.predict(steps=steps_ahead)
+
+        # Get forecasted mean values
+        forecast_mean = forecast.predicted_mean
+
+        # Get forecast dates
+        forecast_index = pd.date_range(start=t, periods=steps_ahead + 1, freq='B')[1:]
+
+        daily_forecasts = {'Date' : t}
+        # Collect forecasts for specified horizons
+        for h in forecast_horizons:
+            if h - 1 < len(forecast_mean):
+                forecast_value = forecast_mean.iloc[h - 1]
+                # Get actual value if available
+                daily_forecasts[f'{h}'] = forecast_value
+            else:
+                break
+
+        forecasts.append(daily_forecasts)
+
+    
+    forecasts_df = pd.DataFrame(forecasts)
+    forecasts_df.to_csv('ARMA Forecasts', index=True)
+    return forecasts_df
+
+
 
 # Run the main function
 if __name__ == '__main__':
-    # Getting data
-    # data = prepare_data(start_date='2004-01-01', end_date='2023-12-31')
-    # data.to_csv('December 31, 2023', index = True)
+    parser = argparse.ArgumentParser()
 
-    # Refitting the model
+    parser.add_argument('-D', '--data', action='store_true', help='Retrieve Latest VIX Data')
+
+    args = parser.parse_args()
+
+    if args.data:
+        get_latest_data()
+        print(f'Data stored in file')
+    
     vix_data = pd.read_csv('December 31, 2023', index_col=0)
     data = HAR_data_preparation(vix_data)
+
+    # Refitting the model
     data.to_csv('HAR_Data_2004-2023', index=True)
     # model = train_ARMA_model(vix_data[vix_data.index < '2013-12-31'].Close)
 
-    # Load ARMA model
-    model = load_ARMA_model()
+    # # Load ARMA model
+    # model = load_ARMA_model()
 
-    # Generate Forecasts
-    generate_forecasts(vix_data.Close, '2014-01-01', '2014-06-01')
+    # # Generate Forecasts
+    # generate_forecasts(vix_data.Close, '2014-01-01', '2014-06-01')
+
+    # Fit HAR model
+    har_model = fit_har_model(data)
