8b382ce083dadf1d859518278b841486b7347fc4
christopheradolphe <ckwadolphe@gmail.com>
2024-10-03 23:44:44 -0400

    Put data retrieval functions in a separate file

--- ARMA Model/ARMA.py
+++ ARMA Model/ARMA.py
@@ -7,58 +7,7 @@ from pandas_datareader import data as web
 import pickle
 import statsmodels.api as sm
 import argparse
-
-def load_vix_data(start_date='2004-01-01', end_date='2023-12-31'):
-    vix_data = yf.download('^VIX', start=start_date, end=end_date)
-    vix_data = vix_data['Close'].dropna()  # Keep only the 'Close' column and drop NaN values
-    return vix_data
-
-def load_sp500_data(start_date='2004-01-01', end_date='2023-12-31'):
-    adjusted_start_date = (pd.to_datetime(start_date) - pd.tseries.offsets.BDay(1))
-    sp500_data = yf.download('^GSPC', start=adjusted_start_date, end=end_date)
-    sp500_data = sp500_data[['Close', 'Volume']].dropna()
-    sp500_data['S&P Returns'] = sp500_data['Close'].pct_change()
-    return sp500_data.drop(columns = ['Close']).drop(sp500_data.index[0])
-
-def load_term_spread(start_date='2004-01-01', end_date='2023-12-31'):
-    dgs10 = web.DataReader('DGS10', 'fred', start_date, end_date)
-    dgs2 = web.DataReader('DGS2', 'fred', start_date, end_date)
-    term_spread = dgs10['DGS10'] - dgs2['DGS2']
-    term_spread = term_spread.dropna()
-    term_spread.name = 'TermSpread'
-    return term_spread
-
-def HAR_data_preparation(data):
-    # Compute lagged VIX variables
-    data['VIX_t-1'] = data['Close'].shift(1)
-    data['VIX_t-5'] = data['Close'].rolling(window=5).mean().shift(1)
-    data['VIX_t-22'] = data['Close'].rolling(window=22).mean().shift(1)
-    
-    # Compute lagged exogenous variables
-    data['S&P Returns_t-1'] = data['S&P Returns'].shift(1)
-    data['Volume_t-1'] = data['Volume'].shift(1)
-    data['TermSpread_t-1'] = data['TermSpread'].shift(1)
-    
-    # Drop rows with NaN values
-    data = data.dropna()
-    return data
-
-def get_latest_data(start_date='2004-01-01', end_date='2023-12-31'):
-    # Load data
-    vix_data = load_vix_data(start_date, end_date)
-    sp500_data = load_sp500_data(start_date, end_date)
-    term_spread = load_term_spread(start_date, end_date)
-    
-    # Combine data into a single DataFrame
-    data = pd.concat([vix_data, sp500_data, term_spread], axis = 1)
-    data = HAR_data_preparation(data)
-    
-    # Drop missing values
-    data = data.dropna()
-    filename = f'Latest_VIX_Data'
-    data.to_csv(filename, index = True)
-    print(f'Latest VIX Data Retrieved and Stored in {filename}.csv')
-    return data
+import data_retriever
 
 def train_ARMA_model(train_data):
     model = ARIMA(train_data, order=(2,0,2))
@@ -166,14 +115,12 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.data:
-        get_latest_data()
+        data_retriever.get_latest_data()
         print(f'Data stored in file')
     
-    vix_data = pd.read_csv('December 31, 2023', index_col=0)
-    data = HAR_data_preparation(vix_data)
+    
 
     # Refitting the model
-    data.to_csv('HAR_Data_2004-2023', index=True)
     # model = train_ARMA_model(vix_data[vix_data.index < '2013-12-31'].Close)
 
     # # Load ARMA model
