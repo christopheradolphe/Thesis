6a7c41f008e59af93994a79fa78d33caabde9d18
christopheradolphe <ckwadolphe@gmail.com>
2024-11-01 11:51:43 -0400

    Create template for SVR forecasting

---
+++ ARMA Model/svr.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+from sklearn.svm import SVR
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+import matplotlib.pyplot as plt
